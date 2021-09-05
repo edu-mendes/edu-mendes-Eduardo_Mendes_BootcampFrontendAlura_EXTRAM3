b15e59bb2682ee327ee16725dc2ce808d0c04b85
edu-mendes <eduardomendesnorberto@outlook.com>
2021-09-05 18:37:12 -0300

    Ateracao para componente funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './styles/index.scss'
+import './styles/index.css'
 
 import Raca from './components/Raca'
 import ListaRacas from './components/ListaRacas'
@@ -7,66 +7,60 @@ import Cabecalho from './components/Cabecalho'
 
 import { buscaSobreRacas, buscaImagemPorRaca, buscaTodasRacas } from './api'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      racas: [],
-      racaSelecionada: {},
-      status: 'Você ainda não selecionou nenhum cachorro :('
-    }
 
-  }
+const App = () => {
+  const [racas, setRacas] = React.useState([]);
+  const [racaSelecionada, setRacaSelecionada] = React.useState({});
+  const [status, setStatus] = React.useState('Você ainda não selecionou nenhum cachorro :(')
 
-  componentDidMount() {
-    this.carregaInformacoesIniciais()
-  }
+  React.useEffect(() => {
+    carregaInformacoesIniciais()
+  });
 
-  carregaInformacoesIniciais() {
+  const carregaInformacoesIniciais = () => {
     buscaSobreRacas()
       .then(informacoes => {
-        this.carregaListaRacas(informacoes)
+        carregaListaRacas(informacoes)
       })
-      .catch(erro => this.setState({
-        status: 'Oops, algo deu errado no carregamento da página. Pode tentar novamente?'
-      }))
+      .catch(erro => setStatus( 'Oops, algo deu errado no carregamento da página. Pode tentar novamente?'))
     }
 
-    carregaListaRacas(sobreRacas) {
+    const carregaListaRacas = (sobreRacas) => {
       buscaTodasRacas()
         .then(racas => {
           const listaRacasMostradas = sobreRacas.filter(
             sobre => racas.includes(sobre.name.toLowerCase())
           )
-          this.setState({ racas: [...listaRacasMostradas] })
+          setRacas( [...listaRacasMostradas] )
         })
     }
 
-    selecionaRaca = raca => {
-      const infoSelecionada = this.state.racas.filter(infoRaca => infoRaca.name === raca)
+   const selecionaRaca = raca => {
+      const infoSelecionada = racas.filter(infoRaca => infoRaca.name === raca)
 
       buscaImagemPorRaca(raca)
-        .then(imagem => this.setState({
-          racaSelecionada: {...this.state.racaSelecionada, imagem, ...infoSelecionada[0]},
-          status: 'A imagem sempre será diferente, pode clicar quantas vezes quiser!'
-        }))
+        .then(imagem => setRacaSelecionada(
+          {...racaSelecionada, imagem, ...infoSelecionada[0]},
+        ))
+        .then(status => setStatus(
+          'A imagem sempre será diferente, pode clicar quantas vezes quiser!'
+        ))
         .catch(erro => {
           const eh404 = erro.response.status === 404
           const mensagem = eh404 ? 'Não encontramos essa raça :(' : 'Oops, algo deu errado. Pode tentar novamente?'
 
-          this.setState({status: mensagem})
+          setStatus(mensagem)
         })
   }
 
-  render() {
-    return (
-      <div className="container">
-        <Cabecalho status={this.state.status} />
-        <Raca raca={this.state.racaSelecionada} />
-        <ListaRacas racas={this.state.racas} selecionaRaca={this.selecionaRaca} />
-      </div>
-    )
-  }
-}
+  return (
+    <div className="container">
+      <Cabecalho status={status} />
+      <Raca raca={racaSelecionada} />
+      <ListaRacas racas={racas} selecionaRaca={selecionaRaca} />
+    </div>
+  )
+
+};
 
 export default App
